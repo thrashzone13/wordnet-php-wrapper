dcea5c5e8994f1d4e211aa2bc07cd74b16bc69d4
thrashzone13 <reza.rabbani.369@gmail.com>
2021-08-27 13:24:16 +0430

    :pencil2: change duplicate function names

--- helper.php
+++ helper.php
@@ -5,12 +5,12 @@ function str_between(string $string, string $start, string $end): string
     return strstr(ltrim(strstr($string, $start), $start), $end, true);
 }
 
-function str_starts_with(string $string, string $needle): bool
+function str_start_with(string $string, string $needle): bool
 {
     return substr($string, 0, strlen($needle)) === $needle;
 }
 
-function str_ends_with(string $string, string $needle): bool
+function str_end_with(string $string, string $needle): bool
 {
     $length = strlen($needle);
     return !$length || substr($string, -$length) === $needle;
@@ -18,5 +18,5 @@ function str_ends_with(string $string, string $needle): bool
 
 function str_is_between(string $string, string $start, string $end): bool
 {
-    return str_starts_with($string, $start) && str_ends_with($string, $end);
+    return str_start_with($string, $start) && str_end_with($string, $end);
 }
