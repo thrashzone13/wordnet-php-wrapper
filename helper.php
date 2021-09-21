f0e611cdda9fc05a19f5829b21cf4a0e7ef4c5ff
thrashzone13 <reza.rabbani.369@gmail.com>
2021-09-21 12:16:11 +0430

    :eyes: implement helper functions safely

--- helper.php
+++ helper.php
@@ -1,22 +1,30 @@
 <?php
 
-function str_between(string $string, string $start, string $end): string
-{
-    return strstr(ltrim(strstr($string, $start), $start), $end, true);
+if (!function_exists('str_between')) {
+    function str_between(string $string, string $start, string $end): string
+    {
+        return strstr(ltrim(strstr($string, $start), $start), $end, true);
+    }
 }
 
-function str_start_with(string $string, string $needle): bool
-{
-    return substr($string, 0, strlen($needle)) === $needle;
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $string, string $needle): bool
+    {
+        return substr($string, 0, strlen($needle)) === $needle;
+    }
+
+}
+if (!function_exists('str_ends_with')) {
+    function str_ends_with(string $string, string $needle): bool
+    {
+        $length = strlen($needle);
+        return !$length || substr($string, -$length) === $needle;
+    }
 }
 
-function str_end_with(string $string, string $needle): bool
-{
-    $length = strlen($needle);
-    return !$length || substr($string, -$length) === $needle;
-}
-
-function str_is_between(string $string, string $start, string $end): bool
-{
-    return str_start_with($string, $start) && str_end_with($string, $end);
+if (!function_exists('str_is_between')) {
+    function str_is_between(string $string, string $start, string $end): bool
+    {
+        return str_starts_with($string, $start) && str_ends_with($string, $end);
+    }
 }
